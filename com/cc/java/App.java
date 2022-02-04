962c1d76d518690adb4f28b71206d5705fbc88ca
Peter <schmidt.multimedia@gmail.com>
2022-02-04 12:41:04 +0100

    Polymorphie / areaDiff(Shape,SShape)

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,11 +4,35 @@ public class App {
 
     public static void main(String[] args) {
   
+        Rectangle rectangle = new Rectangle(10.0, 10.0);
+        Triangle triangle = new Triangle(10.0, 10.0);
+        Circle circle = new Circle(5.0);
 
+        output("------  Flächen ------");
+        output("Fläche Rechteck: " + rectangle.area());
+        output("Fläche Dreieck: " + triangle.area());
+        output("Fläche Kreis: " + circle.area());
 
-        
+        output("------  Differenzen ------");
+        output("Diff. r-t: " + areaDiff(rectangle, triangle));
+        output("Diff. r-c: " + areaDiff(rectangle, circle));
+        output("Diff. c-t: " + areaDiff(circle, triangle));
     }
 
+   //  1.Variante 1000x ggf. überladen ....
+    // private static double areaDiff(Rectangle r, Triangle t) {
+    //     return r.area() - t.area();
+    // }
+
+    // überladene Methode .....
+    // private static double areaDiff(Rectangle r, Circle c) {
+    //     return r.area() - c.area();
+    // }
+
+    // Besser: Polymorphie
+    private static double areaDiff(Shape s1, Shape s2) {
+        return s1.area() - s2.area();
+    }
 
     private static void output(String outputString) {
         System.out.println(outputString);
